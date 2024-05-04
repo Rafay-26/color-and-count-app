e28156cd0e55ae85016392f47528ebd449601459
Rafay-26 <abdulrafay12020@gmail.com>
2024-05-04 17:49:31 +0500

    counter app or color app js sa

---
+++ app.js
@@ -0,0 +1,39 @@
+// function fun(){
+
+//    document.body.style.backgroundColor ="pink"
+
+// }
+// function Red(){
+
+//     document.body.style.backgroundColor ="red"
+
+//  }function Blu(){
+
+//     document.body.style.backgroundColor ="blue"
+
+//  }function Pur(){
+
+//     document.body.style.backgroundColor ="purple"
+
+//  }function Bla(){
+
+//     document.body.style.backgroundColor ="black"
+
+//  }
+
+
+
+
+// function incr(){
+//     var spa = document.querySelector('#count')
+//     var num = parseInt(spa.innerHTML)
+//     num +=1
+//     spa.innerHTML=num 
+// }
+// function dic(){
+//     var spa = document.querySelector('#count')
+//     var num = parseInt(spa.innerHTML)
+//     num +=-1
+//     spa.innerHTML=num 
+// }
+
